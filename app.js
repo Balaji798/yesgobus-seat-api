6fe2bd9c753b12398fcc44719abe2cea3906f906
Balaji Yadav <93571474+Balaji798@users.noreply.github.com>
2024-04-25 17:04:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,12 +26,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: *,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
